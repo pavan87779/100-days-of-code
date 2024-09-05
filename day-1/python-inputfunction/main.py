ab5241f334621fab2f31487381a4faf25401992b
Pavan babu <131483362+pavan87779@users.noreply.github.com>
2024-09-05 13:21:13 +0530

    Add files via upload

---
+++ day-1/python-inputfunction/main.py
@@ -0,0 +1,4 @@
+print(len("Hello"+" "+input("What is your name:")))
+username=input("enter name:")
+length=len(username)
+print(length)
---
+++ day-1/variable-naming/main.py
@@ -0,0 +1,5 @@
+name="pavan"
+#naming variable for user understanding
+name_for_user='pavan'
+print(name)
+print(name_for_user)
