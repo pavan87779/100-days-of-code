bad689cfe7fed39b229fc2d9515fe599f73514c2
Pavan babu <131483362+pavan87779@users.noreply.github.com>
2024-09-07 19:47:04 +0530

    Add files via upload

---
+++ day-6/python-functions/main.py
@@ -0,0 +1,21 @@
+#def keyword is used to for functions
+def my_function():
+    print("Hello!")
+
+my_function()
+
+#return function
+def my_func():
+    return "Hello world!"
+
+print(my_func())
+
+#function Indentation
+def fun(sky):
+    if sky=="clear":
+        print("blue")
+    elif sky=="cloudy":
+        print("grey")
+    print("Hello")
+
+fun("clear")
